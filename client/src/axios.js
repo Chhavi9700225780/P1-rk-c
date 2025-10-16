171c9d2ba1af3501d4467d8c53f16c1c02ba9be5
Chhavi9700225780 <c9081741@gmail.com>
2025-10-16 21:38:36 +0530

    done

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL ||"http://localhost:5000";
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 axios.defaults.withCredentials = true; // important to send/receive cookies
 export default axios;
